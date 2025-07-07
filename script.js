44ed36164a6abe1f236ada12252e1642efacbc0d
Pndr23 <paolamuratori.pm@gmail.com>
2025-07-07 16:32:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ function addButton(text, checkFn) {
        displayCurrentCard(currentCard);
       setTimeout(() => {
         generateChallenge();   // ✅ e poi nuova sfida
-      }, 1000);
+      }, 2000);
     }
   };
 
