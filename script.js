b8a8ce17aa25fc1e6a9140dd0f70096391ebe4eb
Pndr23 <paolamuratori.pm@gmail.com>
2025-08-25 12:32:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -139,6 +139,7 @@ function showMinigiocoJolly(callback) {
     gameArea.style.display = gameAreaOriginalDisplay;
     window.removeEventListener("resize", resizeMinigioco);
   };
+  } 
 function aggiornaMoltiplicatori() {
   const livello = document.getElementById("risk").value;
   console.log("aggiornaMoltiplicatori chiamata, livello:", livello);
